155b00aabc52d1602667728f49f082851d7b6d97
Karishad <44437832+Karishad@users.noreply.github.com>
2021-12-14 15:59:40 -0600

    Laptop Dying :8ball:
    Go on without me desktop, the end is neigh for me.
    
    Need charger

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <stdio.h>
 #include <string>
+#include <array>
 
 using namespace std;
 
@@ -9,16 +10,30 @@ int main()
 {
     fstream fs;
     fs.open("day1.txt", fstream::out);
+    int counter = 0;
     if (fs.is_open())
     {
+        int previous = 0;
+        int current = 0;
+        while (true)
+        {
+            fs >> current;
+            if (current > previous)
+                counter++;
+            if (fs.eof())
+                break;
+            previous = current;
+        }
     }
+
+    // In case fstream fails
     else
     {
         fs.close();
         cout << "day1.txt not found or file failed to open correctly." << endl;
         return -1;
     }
-
+    cout << counter << endl;
     fs.close();
     return 0;
 }
