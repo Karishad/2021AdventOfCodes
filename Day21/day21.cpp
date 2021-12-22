687df66ce99c484537810c940d505e7a002838ff
Karishad <44437832+Karishad@users.noreply.github.com>
2021-12-21 18:50:07 -0600

    Started Day 21
    Beginning Day 21 implementation in C++

---
+++ Day21/day21.cpp
@@ -0,0 +1,29 @@
+// I'm too lazy so I will manually input the starting positions
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numberofrolls = 0;
+    int player1score = 0;
+    int player1position = 3; // Starting position is 3
+    int player2score = 0;
+    int player2position = 5; // Starting position is 5
+    int boardposition = 0;
+
+    while (player1score < 1000 || player2score < 1000)
+    {
+        if (numberofrolls % 2 == 0)
+        {
+            // Player 1 goes
+        }
+        else
+        {
+            // Player 2 goes
+        }
+        break;
+    }
+    return 0;
+}
