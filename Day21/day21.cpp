ae627ed0c61d09a16c927c70d77fa91615ef5ca7
Karishad <44437832+Karishad@users.noreply.github.com>
2021-12-22 15:36:23 -0600

    Completed day21.cpp

--- Day21/day21.cpp
+++ Day21/day21.cpp
@@ -13,17 +13,43 @@ int main()
     int player2position = 5; // Starting position is 5
     int boardposition = 0;
 
-    while (player1score < 1000 || player2score < 1000)
+    int incrdice = 1;
+
+    while (player1score < 1000 && player2score < 1000)
     {
         if (numberofrolls % 2 == 0)
         {
             // Player 1 goes
+            for (int i = 3; i > 0; i--)
+            { // Iterates over 3 rolls
+                player1position += incrdice;
+                while (player1position > 10)
+                {
+                    player1position -= 10;
+                }
+                incrdice++;
+                numberofrolls++;
+            }
+            player1score += player1position;
         }
         else
         {
             // Player 2 goes
+            for (int i = 3; i > 0; i--)
+            { // Iterates over 3 rolls
+                player2position += incrdice;
+                while (player2position > 10)
+                {
+                    player2position -= 10;
+                }
+                incrdice++;
+                numberofrolls++;
+            }
+            player2score += player2position;
         }
-        break;
     }
+    cout << player1score << endl;
+    cout << player2score << endl;
+    cout << numberofrolls << endl;
     return 0;
 }
